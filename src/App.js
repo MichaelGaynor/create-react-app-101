596e1234e3d1fbe148f553c3039a35aae7ef031c
Michael Gaynor <jmichaelgaynor@gmail.com>
2017-06-27 12:06:33 -0400

    Added poster array to return

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import $ from 'jquery';
+import Poster from './Poster';
 
 class App extends Component {
 
     constructor(props) {
         super(props);
+
         /* We define state variables by putting them in the constructor.
         We define it as an object on this.state. */
+
         this.state = {
             moviePosters: []
         }
@@ -17,25 +20,33 @@ class App extends Component {
         console.log("I'm in the DOM")
         var url = 'http://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5';
         $.getJSON(url, (movieData)=>{
-            console.log(movieData)
+
             /* Changing the state will trigger a re-render */
+
             this.setState({
                 moviePosters: movieData.results
             })
+
             /*BAD-> this.state.moviePosters = movieData.results is VERY BAD.*/
+
         })
     }
 
     render() {
         var postersArray = [];
+
+        /* First time through render this will be an empty array.
+        Second time through (after componentDidMount runs) it won't be empty. */
+
         this.state.moviePosters.map((poster,index)=>{
-            console.log(poster);
+            postersArray.push(<Poster poster={poster} key={index}/>)
             return
         });
 
         return (
             <div className="App">
                 <h1>This is the movie app (again)</h1>
+                {postersArray}
             </div>
         );
     }
---
+++ src/Poster.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Poster extends Component{
+    render(){
+        return(
+            <h1>Movie Here</h1>
+        )
+    }
+}
+
+export default Poster;
