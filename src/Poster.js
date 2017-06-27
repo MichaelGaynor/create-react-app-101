6497b57686d164eb77568d8023d80fe21cb0d4d1
Michael Gaynor <jmichaelgaynor@gmail.com>
2017-06-27 12:17:20 -0400

    Posters appear

--- src/Poster.js
+++ src/Poster.js
@@ -2,8 +2,12 @@ import React, { Component } from "react";
 
 class Poster extends Component{
     render(){
+        var imagePath = 'http://image.tmdb.org/t/p/w300'+this.props.poster.poster_path;
         return(
-            <h1>Movie Here</h1>
+            <div className="single-movie col-sm-3">
+                <h1>Movie Here</h1>
+                <img src={imagePath}/>
+            </div>
         )
     }
 }
